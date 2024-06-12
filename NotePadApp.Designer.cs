31a84a3924eec31e0d4bcb15501b4b7af2d63e96
Unknow290 <xxneo290xx@gmail.com>
2024-06-13 09:10:03 +1200

    UI Creation Done

--- NotePadApp.Designer.cs
+++ NotePadApp.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NotePad
 {
-    partial class Form1
+    partial class NotePad
     {
         /// <summary>
         /// Required designer variable.
@@ -29,17 +29,15 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.ToolMenu = new System.Windows.Forms.ToolStripMenuItem();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.New = new System.Windows.Forms.ToolStripMenuItem();
             this.Open = new System.Windows.Forms.ToolStripMenuItem();
-            this.Save_As = new System.Windows.Forms.ToolStripMenuItem();
             this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save_As = new System.Windows.Forms.ToolStripMenuItem();
             this.Print = new System.Windows.Forms.ToolStripMenuItem();
             this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -49,17 +47,10 @@
             this.ToolMenu});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1007, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(755, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(6, 21);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(995, 524);
-            this.textBox1.TabIndex = 1;
             // 
             // ToolMenu
             // 
@@ -71,75 +62,74 @@
             this.Print,
             this.Exit});
             this.ToolMenu.Name = "ToolMenu";
-            this.ToolMenu.Size = new System.Drawing.Size(60, 26);
-            this.ToolMenu.Text = "Menu";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(0, 31);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1007, 577);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.ToolMenu.Size = new System.Drawing.Size(37, 20);
+            this.ToolMenu.Text = "File";
             // 
             // New
             // 
             this.New.Name = "New";
             this.New.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.New.Size = new System.Drawing.Size(233, 26);
+            this.New.Size = new System.Drawing.Size(186, 22);
             this.New.Text = "New";
             // 
             // Open
             // 
             this.Open.Name = "Open";
             this.Open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.Open.Size = new System.Drawing.Size(233, 26);
+            this.Open.Size = new System.Drawing.Size(186, 22);
             this.Open.Text = "Open";
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.Save.Size = new System.Drawing.Size(186, 22);
+            this.Save.Text = "Save";
             // 
             // Save_As
             // 
             this.Save_As.Name = "Save_As";
             this.Save_As.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
             | System.Windows.Forms.Keys.S)));
-            this.Save_As.Size = new System.Drawing.Size(233, 26);
+            this.Save_As.Size = new System.Drawing.Size(186, 22);
             this.Save_As.Text = "Save As";
-            // 
-            // Save
-            // 
-            this.Save.Name = "Save";
-            this.Save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.Save.Size = new System.Drawing.Size(233, 26);
-            this.Save.Text = "Save";
             // 
             // Print
             // 
             this.Print.Name = "Print";
             this.Print.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
-            this.Print.Size = new System.Drawing.Size(233, 26);
+            this.Print.Size = new System.Drawing.Size(186, 22);
             this.Print.Text = "Print";
             // 
             // Exit
             // 
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(233, 26);
+            this.Exit.Size = new System.Drawing.Size(186, 22);
             this.Exit.Text = "Exit";
             // 
-            // Form1
+            // richTextBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(755, 454);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // NotePad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1007, 588);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(755, 478);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "NotePad";
+            this.Text = "NotePad";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,14 +139,13 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem ToolMenu;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ToolStripMenuItem New;
         private System.Windows.Forms.ToolStripMenuItem Open;
         private System.Windows.Forms.ToolStripMenuItem Save;
         private System.Windows.Forms.ToolStripMenuItem Save_As;
         private System.Windows.Forms.ToolStripMenuItem Print;
         private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- NotePadApp.cs
+++ NotePadApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace NotePad
 {
-    public partial class Form1 : Form
+    public partial class NotePad : Form
     {
-        public Form1()
+        public NotePad()
         {
             InitializeComponent();
         }

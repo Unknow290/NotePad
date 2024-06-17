d37c19304e55cb197587b01b02837cb5ab7d7468
xsmyyy <samuel.joe.aim@gmail.com>
2024-06-18 09:18:37 +1200

    Sprint 2 Code

--- NotePadApp.cs
+++ NotePadApp.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace NotePad // notepad
 {
     public partial class NotePad : Form
     {
         private bool isSaved;
+        private string currentFilePath;
         public NotePad()
         {
             InitializeComponent();
@@ -20,49 +23,50 @@ namespace NotePad // notepad
 
         private void New_Click(object sender, EventArgs e)
         {
-            if (!isSaved)
+            if (!isSaved) // checks to see if you want to save your work
             {
                 var result = MessageBox.Show("You have unsaved changes. Do you want to save them before creating a new note?", "Unsaved Changes", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
 
                 if (result == DialogResult.Yes)
                 {
+                    SaveFile();
                     NewNote();
                 }
                 else if (result == DialogResult.No)
                 {
-                    NewNote(); 
+                    NewNote();
                 }
             }
             else
             {
-                NewNote(); 
+                NewNote();
             }
         }
-
-        private void NewNote()
+        private void Open_Click(object sender, EventArgs e) // opens an existing file
         {
-            NotePad np = new NotePad();
-            np.Show();
-        }
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = ofd.FileName;
+                using (StreamReader sr = new StreamReader(currentFilePath))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+                isSaved = true;
+            }
 
-        private void Open_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog(); 
-            ofd.ShowDialog();
         }
 
         private void Save_Click(object sender, EventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.ShowDialog();
+            SaveFile();
         }
 
         private void Save_As_Click(object sender, EventArgs e)
         {
-            SaveFileDialog sfd2 = new SaveFileDialog();
-            sfd2.ShowDialog();
+            SaveAsFile();
         }
-
         private void Print_Click(object sender, EventArgs e)
         {
             PrintDialog pd = new PrintDialog();
@@ -72,6 +76,67 @@ namespace NotePad // notepad
         private void Exit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void NewNote()
+        {
+            NotePad np = new NotePad();
+            np.Show();
+        }
+        private void SaveAsFile() // Saves as / code behind the save file
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = sfd.FileName;
+                SaveFile(currentFilePath);
+            }
+        }
+        private void SaveFile(string filePath = null) // saves file
+        {
+            if (filePath == null)
+            {
+                SaveAsFile();
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+                isSaved = true;
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo(); // undo
+            undoToolStripMenuItem.Enabled = false;
+            redoToolStripMenuItem.Enabled = true;
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo(); // redo
+            undoToolStripMenuItem.Enabled = true;
+            redoToolStripMenuItem.Enabled = false;
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText); //cut
+            richTextBox1.SelectedText = string.Empty;
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText); //copy
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string xx = Clipboard.GetText();  //paste
+            richTextBox1.Text = richTextBox1.Text.Insert(richTextBox1.SelectionStart, xx);
         }
     }
 }
